9e2e63755602a5e999eb250469dd1c9f3c0f14f7
Nathan <nslee333@gmail.com>
2022-12-14 22:10:40 -0800

    More work on Http routes.

--- rest_api/index.js
+++ rest_api/index.js
@@ -31,6 +31,14 @@ app.get("/api/posts/:year/:month", (req, res) => {
     res.send(req.query);
 });
 
+app.get("/api/courses/:id", (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The course with the given id was not found.');
+
+    res.send(course);
+    
+});
+
 
 const port = process.send.PORT || 3013;
 app.listen(port, () => console.log(`Listening on port ${port} ...`));

221a06e6b1ffa20814d4dcd155aee4208ca1b5a7
Nathan <nslee333@gmail.com>
2022-12-17 18:06:55 -0800

    task_app2:[backend]: Initialized a new project for task_app in attempt to fix issues.

---
+++ task_app2/backend/src/controller.ts
@@ -0,0 +1,15 @@
+import { fetchCollection } from "./db"
+
+export const getDocuments = async () => {
+    try {
+        const result =  await fetchCollection();
+        return result;
+    } catch (error) {
+        console.error(error);
+    }
+    
+}
+
+// console.log(getDocuments());
+
+module.exports = getDocuments;
---
+++ task_app2/backend/src/db.ts
@@ -0,0 +1,60 @@
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(process.env.URI, {useNewUrlParser: true});
+import {ObjectId} from 'mongodb';
+
+ // Function to fetch all current tasks.
+export const fetchCollection = async () => {
+    try {
+        client.connect(async (err: Error) => {
+            if (err) {
+                return console.log("Unable to connect to database.")
+            }
+            const taskCollection = client.db("task_app").collection("tasks");
+           
+            const result = await taskCollection.find();
+           
+            client.close();
+            return result;
+        });
+
+    } catch (error) {
+        console.error(error);
+        return error;
+    }
+}
+
+// Function to add a new task to the collection.
+export const addTask = async (inputTask: string) => {
+    try {
+        client.connect(async (err: Error) => {
+            const taskCollection = client.db("task_app").collection("tasks");
+
+            const taskDocument = {
+                task: inputTask,
+            }
+
+            await taskCollection.insertOne(taskDocument);
+            client.close();
+        });
+        
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// Delete a task from the database.
+export const deleteTask = async (deleteId: ObjectId ) => {
+    try {
+        client.connect(async (err: Error) => {
+            const taskCollection = client.db("task_app").collection("tasks");
+            await taskCollection.deleteOne({_id: deleteId});
+            client.close();
+        })
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+module.exports = {fetchCollection, addTask, deleteTask}

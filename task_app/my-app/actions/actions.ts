58e37a9138e37bccbff0641738857997022fe30e
Nathan <nslee333@gmail.com>
2022-12-22 08:01:47 -0800

    task_app:frontend: wrote axios http methods, updated packages.

--- task_app/my-app/actions/actions.ts
+++ task_app/my-app/actions/actions.ts
@@ -1,9 +1,43 @@
-import axios from 'axios';
+import axios, { AxiosResponse, AxiosError } from 'axios';
 
 // GET
+export const getMethod = await axios.get('/api/')
+    .then(function (response: AxiosResponse) {
+        console.log(response);
+        return response;
+    })
+    .catch(function (error: AxiosError) {
+        console.log(error)
+    })
+    .then(function () {
+        // Always executed.
+    });
 
 // POST
+export const postMethod = await axios.post('/api/')
+    .then(function (response: AxiosResponse) {
+        console.log(response);
+        return response;
+    })
+    .catch(function (error: AxiosError): void {
+        console.log(error);
+    })
+    .then(function (): void {
+        // Always executed.
+    })
 
 // DELETE
-
-// Export functions.
+export const deleteMethod = await axios.delete("/api/")
+    .then(function (response: AxiosResponse) {
+        console.log(response);
+        return response;
+    })
+    .catch(function (error: AxiosError): void {
+        console.log(error);
+    })
+    .then(function (): void {
+        // Always executed.
+    })
+ 
+ 
+module.exports = { getMethod, postMethod, deleteMethod }

3872dee853d7404bfc3fdc661039a3c3b3dde4f6
Nathan <nslee333@gmail.com>
2022-12-30 15:57:10 -0800

    Projects_: Chore: Removed reference directories not in use

--- goal_tracker/backend/routes/goalRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express, {Express, Request, Response} from 'express';
-const router = express.Router();
-const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
-
-router.route('/').get(getGoals).post(setGoal);
-
-router.route('/:id').delete(deleteGoal).put(updateGoal);
-
-module.exports = router;

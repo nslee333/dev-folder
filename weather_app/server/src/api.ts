fff42e177b8bbc33bf8c236c54008d071b1f35d2
Nathan <nslee333@gmail.com>
2022-12-30 23:50:34 -0800

    weather_app:[server]: Writing API calls to WeatherAPI.com

--- weather_app/server/src/api.ts
+++ weather_app/server/src/api.ts
@@ -1,8 +1,49 @@
-import axios from 'axios';
+import axios, {AxiosResponse} from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
 
 // Realtime axios request
+const baseURL: string = 'https://api.weatherapi.com/v1/';
+const realtimeAPIMethod: string = '/current.json';
+const forecastAPIMethod: string = '/forecast.json';
+let baseQuery: string | number = 10001;
 
-// Forcast axios request
+export const updateQueryParams = (newQueryParams: string | number) => {
+    baseQuery = newQueryParams;
+}
+
+
+export const realtimeRequest: () => Promise<AxiosResponse | Error> = async () => {
+    const result = await axios.get(baseURL + realtimeAPIMethod, {
+        params: {
+            key: process.env.API_KEY,
+            q: baseQuery
+        }
+    })
+    .then(function (response: AxiosResponse) {
+        return response
+    })
+    .catch(function (error: string) {
+        return new Error(error);
+    })
+    return result;
+}
+
+// Forecast axios request
+export const forecastRequest = async () => {
+    
+}
+
+
+// https://api.weatherapi.com/v1/current.json?key=<>q=97702 - query for 97702, current api.
+
+// Maybe What I'll need to do is sort out the specific data in api.ts, then give it back
+// to server.ts
+
+
+
+
+
 
 // export Forcast and Realtime axios requests
 

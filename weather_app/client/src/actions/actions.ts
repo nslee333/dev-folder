3b7c223a9c797318fbca9d0d790b5c51aa3d9aea
Nathan <nslee333@gmail.com>
2023-01-03 03:29:00 -0800

    [WIP]: Client-side Axios requests.

--- weather_app/client/src/actions/actions.ts
+++ weather_app/client/src/actions/actions.ts
@@ -1,9 +1,49 @@
-import axios from 'axios';
+import axios, { Axios, AxiosError, AxiosResponse } from 'axios';
+
+const baseURL: string = 'http://localhost:1300/api/'
 
 
-// Current weather
+export const realtimeRequest: () => Promise<AxiosResponse | AxiosError> = async () => {
+    const result: AxiosResponse | AxiosError = await axios.get(baseURL + 'realtime', {})
+    .then(function (response: AxiosResponse) {
+        return response;
+    })
+    .catch(function (error) {
+        return error;
+    })
 
-// Hourly forcast
+    return result;
+} 
 
-// 10 day forcast
+// 5 day forecast 
+export const weeklyRequest: () => Promise<AxiosResponse | AxiosError> = async () => {
+    const result: AxiosResponse | AxiosError = await axios.get(baseURL + 'weeklyForecast', {})
+    .then(function (response: AxiosResponse) {
+        return response;
+    })
+    .catch(function (error: AxiosError) {
+        return error;
+    });
+    
+    return result;
+}
 
+// Hourly forecast
+export const hourlyForecast: () => Promise<AxiosResponse | AxiosError> = async () => {
+    const result: AxiosResponse | AxiosError = await axios.get(baseURL + 'hourlyForecast', {})
+    .then(function (response: AxiosResponse) {
+        return response;
+    })
+    .catch(function (error: AxiosError) {
+        return error;
+    })
+
+    return result;
+}
+
+// Settings call
+
+module.exports = {
+    realtimeRequest,
+    weeklyRequest,
+}

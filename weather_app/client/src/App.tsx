758b1502d526da7f492379bd7af753c0dc473846
Nathan <nslee333@gmail.com>
2023-01-03 21:12:05 -0800

    client: Working on skeleton of client.

--- weather_app/client/src/App.tsx
+++ weather_app/client/src/App.tsx
@@ -2,10 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+console.log(window.innerWidth, window.innerHeight)
+
 function App() {
   return (
-    <div></div>
+    <div className='App'>
+      <div className='forecastDiv'></div>
+      <div className='navBar'></div>
+      <div className='realtimeDiv'></div>
+      <div className='hourForecast'></div>
+    </div>
   );
 }
 
 export default App;
+// 1152 x 1920

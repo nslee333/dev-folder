f3daf45445b772b6232e84d74440035481ccc098
Nathan <nslee333@gmail.com>
2023-01-04 12:48:37 -0800

    Added typings file for response data.

---
+++ weather_app/client/src/types/dataTypings.ts
@@ -0,0 +1,34 @@
+export type realtimeWeatherData = {
+    weatherDescription: string,
+    weatherIcon: number,
+    hasPrecipitation: boolean,
+    precipitationType: string | null,
+    temperature: number,
+}
+
+export type forecastDailyData = {
+    date: string,
+    dateEpoch: number,
+    maxTemp: number,
+    minTemp: number,
+    dayIcon: number,
+    dayIconPhrase: string,
+    dayHasPrecipitation: boolean | null,
+    dayPrecipitationType: string | null,
+    dayPrecipitationIntensity: string | null,
+    nightIcon: number,
+    nightIconPhrase: string,
+    nightHasPrecipitation: boolean | null,
+    nightPrecipitationType: string | null,
+    nightPrecipitationIntensity: string | null;
+}
+
+export type forecastHourlyData = {
+    timeEpoch: number,
+    weatherIcon: number,
+    iconPhrase: string,
+    hasPrecipitation: boolean;
+    temperature: number,
+    precipitationType: string | null,
+    precipitationIntensity: string | null,
+}

6d1a59b2058a3f8738f5a06a595c8d186b856bc1
Nathan <nslee333@gmail.com>
2023-01-23 19:30:12 -0800

    Switch to two space indentation.

--- weather_app/client/src/actions/actions.ts
+++ weather_app/client/src/actions/actions.ts
@@ -4,42 +4,42 @@ const baseURL: string = 'http://localhost:1300/api/'
 
 
 export const currentWeather: (location: string, metric: boolean ) => Promise<AxiosResponse | AxiosError | Error> = async (location: string, metric: boolean ) => {
-    const result: AxiosResponse | AxiosError = await axios.post(baseURL + 'current', {
-        "location": location,
-        "metric": metric
-    })
-    .then(function (response: AxiosResponse) {
-        if (response.status === 200) {
-            return response;
+  const result: AxiosResponse | AxiosError = await axios.post(baseURL + 'current', {
+    "location": location,
+    "metric": metric
+  })
+  .then(function (response: AxiosResponse) {
+    if (response.status === 200) {
+      return response;
 
-        } else {
-            return new Error(`Bad response status: ${response.status}, ${response}`);
-        }
-    })
-    .catch(function (error) {
-        return error;
-    })
+    } else {
+      return new Error(`Bad response status: ${response.status}, ${response}`);
+    }
+  })
+  .catch(function (error) {
+      return error;
+  })
 
-    return result;
+  return result;
 } 
 
 
 export const forecastWeather: (location: string, metric: boolean) => Promise<AxiosResponse | AxiosError | Error> = async (location: string, metric: boolean) => {
-    const result: AxiosResponse | AxiosError = await axios.post(baseURL + 'forecast', {
-        "location": location,
-        "metric": metric
-    })
-    .then(function (response: AxiosResponse) {
-        if (response.status === 200) {
-            return response;
+  const result: AxiosResponse | AxiosError = await axios.post(baseURL + 'forecast', {
+    "location": location,
+    "metric": metric
+  })
+  .then(function (response: AxiosResponse) {
+    if (response.status === 200) {
+      return response;
 
-        } else {
-            return new Error(`Bad response status: ${response.status}`), response;
-        }
-    })
-    .catch(function (error: AxiosError) {
-        return error;
-    });
-    
-    return result;
+    } else {
+      return new Error(`Bad response status: ${response.status}`), response;
+    }
+  })
+  .catch(function (error: AxiosError) {
+    return error;
+  });
+  
+  return result;
 }
